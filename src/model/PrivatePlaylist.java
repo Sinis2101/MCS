0e54ff34b77ef387147add98470ef009db0251a4
Sinis2101 <sinisterra2101@gmail.com>
2020-11-05 18:20:43 -0500

    Added new variable to count amount of songs in the playlist. Added method to get info. Started coding a method to add a song to the playlist.

--- src/model/PrivatePlaylist.java
+++ src/model/PrivatePlaylist.java
@@ -2,7 +2,10 @@ package model;
 
 public class PrivatePlaylist extends Playlist {
 
+    private static final int MAX_SONGS = 30;
+
     private User owner;
+    private int songsAmount;
 
     public PrivatePlaylist(String name, User owner) {
 
@@ -11,4 +14,34 @@ public class PrivatePlaylist extends Playlist {
 
     }
 
+    @Override
+    public String getInfo() {
+
+        return  "Name: " + getName() + "\n" +
+                "Duration: " + getDuration() + "\n" +
+                "Genres: ";
+
+
+    }
+
+    @Override
+    public String addSong(Song song, Playlist playlist) {
+
+        boolean added = false;
+
+        for(int i = 0; i < MAX_SONGS && !added; i++) {
+
+            if(getSongs()[i] == null) {
+
+                getSongs()[i] = song;
+                songsAmount ++;
+                added = true;
+
+            }
+
+        }
+
+        return "Song has been added successfully. Press ENTER to continue.";
+
+    }
 }
