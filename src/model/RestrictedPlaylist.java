41f67ad6b43372493d351cdfc04aabae483f4416
Sinis2101 <sinisterra2101@gmail.com>
2020-11-06 16:49:32 -0500

    Added method to grant access to a user. Updated get info method. Added variable to count amount of users.

--- src/model/RestrictedPlaylist.java
+++ src/model/RestrictedPlaylist.java
@@ -2,15 +2,17 @@ package model;
 
 public class RestrictedPlaylist extends Playlist {
 
-    private static final int MAX_USERS = 5;
+    public static final int MAX_USERS = 5;
 
     private User[] users;
+    private int userAmount;
 
     public RestrictedPlaylist(String name, User user) {
 
         super(name);
         users = new User[MAX_USERS];
         users[0] = user;
+        userAmount = 1;
 
     }
 
@@ -22,7 +24,12 @@ public class RestrictedPlaylist extends Playlist {
                 "Duration: " + getDuration() + "\n" +
                 "Genres: " + "\n" +
                 "Type: Restricted" + "\n" +
-                "Users with access: ";
+                "Users with access:" + "\n" +
+                "- " + getUsersList()[0] + "\n" +
+                "- " + getUsersList()[1] + "\n" +
+                "- " + getUsersList()[2] + "\n" +
+                "- " + getUsersList()[3] + "\n" +
+                "- " + getUsersList()[4];
 
     }
 
@@ -47,8 +54,65 @@ public class RestrictedPlaylist extends Playlist {
 
     }
 
+    public String grantAccess(User user, Playlist playlist) {
+
+        boolean isAccessor = false;
+
+        for(User accessor : users) {
+
+            if(accessor == user) {
+
+                isAccessor = true;
+                return "User '" + user.getUsername() + "' has already access to playlist '" + getName() + "' . Press ENTER to go back.";
+
+            }
+
+        }
+
+        boolean added = false;
+
+        for(int i = 0; i < MAX_USERS && !added && !isAccessor; i++) {
+
+            if(users[i] == null) {
+
+                users[i] = user;
+                userAmount ++;
+                added = true;
+
+            }
+
+        }
+
+        return "User '" + user.getUsername() + "' has now access to playlist '" + getName() + "' . Press ENTER to continue.";
+
+    }
+
     // GETTERS
     public User[] getUsers() {
         return users;
     }
+    public int getUserAmount() {
+        return userAmount;
+    }
+    public String[] getUsersList() {
+
+        String[] userList = new String[MAX_USERS];
+
+        for(int i = 0; i < MAX_USERS; i++){
+
+            if(users[i] == null) {
+
+                userList[i] = "";
+
+            } else {
+
+                userList[i] = users[i].getUsername();
+
+            }
+        }
+
+        return userList;
+
+    }
+
 }
